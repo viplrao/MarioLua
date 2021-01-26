a6a0903621d4ac8c53887720a8b99311c17cabdb
Vipul Rao <vipulrao@foresthills.edu>
2021-01-26 15:25:04 -0500

    better sizing of bounds, creationg of DEBUG

--- conf.lua
+++ conf.lua
@@ -1,5 +1,12 @@
 function love.conf(t)
-    t.window.width = 1334
-    t.window.height = 750
-    t.window.title = "Mario"
+    if DEBUG then
+        t.window.width = 1440 --1334
+        t.window.height = 900 --750
+        t.window.title = "Mario- DEBUG ON"
+    else 
+        t.window.width = 1334
+        t.window.height = 750
+        t.window.title = "Mario"
+    end
+
 end
--- main.lua
+++ main.lua
@@ -7,33 +7,35 @@
 -- Most Recent Commit Name:"Added Left, Right Wall"
 
 -- Global, non-UI variables
+-- Before adding, check if they really need to be global...
+DEBUG = false
 LEFT_EDGE_OF_SCREEN = 0
-RIGHT_EDGE_OF_SCREEN = 1220
+RIGHT_EDGE_OF_SCREEN = 1334
 TOP_OF_SCREEN = 0
+BOTTOM_OF_SCREEN  = 750
 WALK_PATH_HEIGHT = 500
-FORCE_TO_APPLY = 400
 
 
 -- Called once, create sprites here
 function love.load()
-    -- Set background color
-    love.graphics.setBackgroundColor(111/255,121/255,255/255)
 
     -- Initalize "world", "stage", whatever you want to call it
     love.physics.setMeter(64)
     world = love.physics.newWorld(0, 9.81*64, true)
-    
+
     -- Create an empty generic objects table
     objects = {}
 
+    -- NOTE: love.physics.newBody uses center point, love.physics.newShape sets dimensions
+
     -- Make an Object for the "Bricks"
-    objects.walk_path = {
-        -- love.physics.newBody uses center point, love.phyisics.shape uses dimensions
-        body = love.physics.newBody(world, RIGHT_EDGE_OF_SCREEN / 2, 700),
-        shape = love.physics.newRectangleShape(RIGHT_EDGE_OF_SCREEN, 140)
+    objects.floor = {
+        body = love.physics.newBody(world, RIGHT_EDGE_OF_SCREEN / 2, 650),
+        shape = love.physics.newRectangleShape(RIGHT_EDGE_OF_SCREEN, 50)
     }
-    objects.walk_path.fixture = love.physics.newFixture(objects.walk_path.body, objects.walk_path.shape)
-    
+    -- Attach body to shape
+    objects.floor.fixture = love.physics.newFixture(objects.floor.body, objects.floor.shape)
+
     -- Make toad
     objects.toad = {
         image = love.graphics.newImage("Assets/tiny toad.png"),
@@ -42,70 +44,86 @@ function love.load()
     }
     -- Give toad density of 0.6 (number may need future tweaking, but good enough)
     objects.toad.fixture = love.physics.newFixture(objects.toad.body, objects.toad.shape, 0.6)
-    -- Add bouncyness
+    -- Add bouncyness, the higher the bouncier
     objects.toad.fixture:setRestitution(0.8)
 
     -- Make left edge, right edge objects
+    local wall_width = 10
+    local wall_height = 2 * BOTTOM_OF_SCREEN
     objects.left_wall = {
-        body = love.physics.newBody(world, LEFT_EDGE_OF_SCREEN, WALK_PATH_HEIGHT),
-        shape = love.physics.newRectangleShape(20, WALK_PATH_HEIGHT),
+        body = love.physics.newBody(world, LEFT_EDGE_OF_SCREEN , WALK_PATH_HEIGHT),
+        shape = love.physics.newRectangleShape(wall_width, wall_height),
     }
     objects.left_wall.fixture = love.physics.newFixture(objects.left_wall.body, objects.left_wall.shape)
 
     objects.right_wall = {
         body = love.physics.newBody(world, RIGHT_EDGE_OF_SCREEN, WALK_PATH_HEIGHT),
-        shape = love.physics.newRectangleShape(20, WALK_PATH_HEIGHT),
+        shape = love.physics.newRectangleShape(wall_width, wall_height),
     }
     objects.right_wall.fixture = love.physics.newFixture(objects.right_wall.body, objects.right_wall.shape)
 end
 
--- Add sprites to the stage here
+-- Add sprites to the world here, re-called everytime love.update() is triggered
 function love.draw()
     -- Add background
     love.graphics.draw(love.graphics.newImage("Assets/mario_no_terrain Cropped.jpg"),0,0)
-    -- Add walk_path object ("floor") to world
-    love.graphics.polygon("line", objects.walk_path.body:getWorldPoints(objects.walk_path.shape:getPoints()))
-    -- Add left wall to world
-    love.graphics.polygon("line", objects.left_wall.body:getWorldPoints(objects.left_wall.shape:getPoints()))
-    -- Add right wall to world
-    love.graphics.polygon("line", objects.right_wall.body:getWorldPoints(objects.right_wall.shape:getPoints()))
+    -- Add boundaries to world, hide if not debugging
+    if DEBUG then
+    love.graphics.polygon("fill", objects.floor.body:getWorldPoints(objects.floor.shape:getPoints()))
+    love.graphics.polygon("fill", objects.left_wall.body:getWorldPoints(objects.left_wall.shape:getPoints()))
+    love.graphics.polygon("fill", objects.right_wall.body:getWorldPoints(objects.right_wall.shape:getPoints()))
+    else
+        love.graphics.polygon("line", objects.floor.body:getWorldPoints(objects.floor.shape:getPoints()))
+        love.graphics.polygon("line", objects.left_wall.body:getWorldPoints(objects.left_wall.shape:getPoints()))
+        love.graphics.polygon("line", objects.right_wall.body:getWorldPoints(objects.right_wall.shape:getPoints()))
+    end
 
-    -- Create Obstacles (very much TODO)
-        for i=1, love.math.random(1,3) do
-            make_new_block(i, false)
-        end
-    -- Add toad object to world
-    love.graphics.draw(objects.toad.image, objects.toad.body:getX(),
-                       objects.toad.body:getY(), objects.toad.shape:getRadius())
+    -- Add toad to world
+    love.graphics.draw(objects.toad.image, objects.toad.body:getX(), objects.toad.body:getY(), objects.toad.shape:getRadius())
 end
 
 -- Called every $dt seconds, do game logic here
 function love.update(dt)
     world:update(dt)
-   -- Checks if right, left, up, down keys are pressed, and calls appropriate step (probably should have been named move) function
-    local keys = {"right","left", "up", "down"}
-    for i=1,#keys do
-       if love.keyboard.isDown(keys[i]) then
-           step(keys[i], FORCE_TO_APPLY)
+
+    -- Checks if right, left, up, down keys are pressed, and calls appropriate step function -- up is a special case
+    local force = 500 -- used with force_keys / physics.applyForce
+    local pixels = 20 -- used with step("up") / body.setY()
+
+    local force_keys = {"right","left", "down" }
+
+    for i=1,#force_keys do
+       if love.keyboard.isDown(force_keys[i]) then
+           step(force_keys[i], force)
          end
     end
+
+    if  love.keyboard.isDown("space") or love.keyboard.isDown("up") then
+        step("up", pixels)
+    end
+    
+    -- esc restart functionality -- WILL NOT REFLECT MODIFIED CODE
+    if love.keyboard.isDown("lctrl", "c") or love.keyboard.isDown("escape") then
+        love.load()
+    end
+
 end
 
--------- Below this line are functions definitons that I have made --------
+-------- Below this line are functions that I have made --------
 
 -- Apply $amount force towards $"direction"
 function step(direction, amount)
     -- up/down don't really work right
     if direction == "up" then
-        objects.toad.body:applyForce(0, amount)
+        objects.toad.body:setY(objects.toad.body:getY() - amount)
     end
 
-    if direction == "down" and (objects.toad.body:getY() + amount) <= WALK_PATH_HEIGHT then
-        objects.toad.body:applyForce(0, -amount)
+    if direction == "down" then
+        objects.toad.body:setY(objects.toad.body:getY() + amount)
     end
 
-    -- left/right do, but need wrap around
-    if direction == "left"  and (objects.toad.body:getX() - amount) >= LEFT_EDGE_OF_SCREEN  then
+    -- left/right work, but need wrap around
+    if direction == "left" then
         objects.toad.body:applyForce(-amount, 0)
     end
 
@@ -116,12 +134,7 @@ end
 
 ------ None of these are used yet -------
 
--- Probably will make new obstacles inside this function, could be a seperate func.?
-function wrap_around()
-    objects.toad.Body:setX(LEFT_EDGE_OF_SCREEN)
-end
-
--- putting this aside to try out physics, but will likely come back
+-- Finds a random number in range of x or y axis
 function rand_on_axis(axis)
     if axis == "x" then
         return love.math.random(LEFT_EDGE_OF_SCREEN, RIGHT_EDGE_OF_SCREEN)
@@ -129,14 +142,4 @@ function rand_on_axis(axis)
     if axis == "y" then
       return love.math.random(TOP_OF_SCREEN, WALK_PATH_HEIGHT)
     end
-end
-
---  this aside too, but likely will NOT come back
-function make_new_block(update)
-    if update == false then
-      --  love.graphics.rectangle("fill", block_x_pos,block_y_pos,100,100)
-    else do
-       -- love.graphics.rectangle("fill", rand_on_axis("x"), rand_on_axis("y"), 100, 100)
-    end
-end
 end
