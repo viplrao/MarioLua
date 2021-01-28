6068965cb9cfedb9c562905529e4c7a70bb3d51c
Vipul Rao <raovipul08@gmail.com>
2021-01-27 21:21:15 -0500

    cleaned up draw_transparent_walls()

--- main.lua
+++ main.lua
@@ -1,8 +1,10 @@
--- Vipul Rao
--- 1/25/20
--- Mario Game (even though there's no Mario...)
--- Usage: love { path to directory, usually just `.` }
--- Once love opens, use the arrow keys to move Toad!
+---------------------------------------------------------
+-- Vipul Rao                                           --
+-- 1/25/20                                             --
+-- Mario Game (even though there's no Mario...)        --
+-- Usage: love { path to directory, usually just `.` } --
+-- Once love opens, use the arrow keys to move Toad!   --
+---------------------------------------------------------
 -- Global, non-UI variables
 -- Before adding, check if they really need to be global...
 DEBUG = false -- if true, boundaries are more marked
@@ -37,7 +39,7 @@ function love.load()
         shape = love.physics.newCircleShape(50)
     }
     objects.toad.fixture = love.physics.newFixture(objects.toad.body,
-                                                   objects.toad.shape, 0.6) -- Set density of 0.6
+                                                   objects.toad.shape, 0.6) -- Set density of Toad to 0.6
     objects.toad.fixture:setRestitution(0.8) -- Add bouncieness to toad, the higher the bouncier
 
     -- Wall sizing
@@ -45,22 +47,24 @@ function love.load()
 
     -- Make left edge, right edge objects
     objects.left_wall = {
-        shape = love.physics.newRectangleShape(wall_width, BOTTOM_OF_SCREEN),
         body = love.physics.newBody(world, LEFT_EDGE_OF_SCREEN,
-                                    BOTTOM_OF_SCREEN / 2, "static")
+                                    BOTTOM_OF_SCREEN / 2, "static"),
+        shape = love.physics.newRectangleShape(wall_width, BOTTOM_OF_SCREEN)
+
     }
     objects.left_wall.fixture = love.physics.newFixture(objects.left_wall.body,
                                                         objects.left_wall.shape)
 
     objects.right_wall = {
-        shape = love.physics.newRectangleShape(wall_width, BOTTOM_OF_SCREEN),
         body = love.physics.newBody(world, RIGHT_EDGE_OF_SCREEN,
-                                    BOTTOM_OF_SCREEN / 2, "static")
+                                    BOTTOM_OF_SCREEN / 2, "static"),
+        shape = love.physics.newRectangleShape(wall_width, BOTTOM_OF_SCREEN)
     }
     objects.right_wall.fixture = love.physics.newFixture(
                                      objects.right_wall.body,
                                      objects.right_wall.shape)
 
+    -- Make a ceiling
     objects.ceiling = {
         body = love.physics.newBody(world, RIGHT_EDGE_OF_SCREEN / 2,
                                     TOP_OF_SCREEN),
@@ -72,11 +76,15 @@ end
 
 -- Add sprites to the world here, re-called everytime love.update() is triggered
 function love.draw()
-    -- Draw background, walls (done in a seperate procedure), toad
+    -- Draw background, walls
     love.graphics.draw(love.graphics.newImage(
                            "Assets/mario_no_terrain Cropped.jpg"),
                        LEFT_EDGE_OF_SCREEN, TOP_OF_SCREEN)
+
+    -- Draw anything that you want hidden
     draw_transparent_walls()
+
+    -- Draw Toad
     love.graphics.draw(objects.toad.image, objects.toad.body:getX(),
                        objects.toad.body:getY(), objects.toad.shape:getRadius())
 end
@@ -85,6 +93,7 @@ end
 function love.update(dt)
     world:update(dt)
 
+    -- How hard to push on Toad?
     local force = 500
 
     -- Check if right, left, up, down keys are pressed, and calls appropriate step function -- up is a special case
@@ -94,20 +103,21 @@ function love.update(dt)
             step(force_keys[i], force)
         end
     end
+
     -- Allow people to use space for up too
     if love.keyboard.isDown("space") then step("up", force) end
+
     -- Hit escape or ctrl-c to restart -- WILL NOT REFLECT MODIFIED CODE!
     if love.keyboard.isDown("lctrl", "c") or love.keyboard.isDown("escape") then
         love.load()
     end
-
 end
 
 -------- Below this line are functions that I have made --------
 
 -- Apply $amount force towards $"direction"
 function step(direction, amount)
-    local anti_gravity_scale_factor = 2
+    local anti_gravity_scale_factor = 2.5
 
     if direction == "up" then
         objects.toad.body:applyForce(0, -amount * anti_gravity_scale_factor)
@@ -124,28 +134,21 @@ end
 
 function draw_transparent_walls()
     -- Override transparency if DEBUGging
-    local mode = "line"
-    local border_transparency = 0
+    local mode = "fill"
     if DEBUG then
-        mode = "fill"
-        border_transparency = 255
+        -- Draw what you need to draw
+        love.graphics.polygon(mode, objects.floor.body:getWorldPoints(
+                                  objects.floor.shape:getPoints()))
+
+        love.graphics.polygon(mode, objects.left_wall.body:getWorldPoints(
+                                  objects.left_wall.shape:getPoints()))
+
+        love.graphics.polygon(mode, objects.right_wall.body:getWorldPoints(
+                                  objects.right_wall.shape:getPoints()))
+
+        love.graphics.polygon(mode, objects.ceiling.body:getWorldPoints(
+                                  objects.ceiling.shape:getPoints()))
     end
-
-    -- Switch to transparent ink
-    love.graphics.setColor(255, 255, 255, border_transparency)
-
-    -- Draw what you need to draw
-    love.graphics.polygon(mode, objects.floor.body:getWorldPoints(
-                              objects.floor.shape:getPoints()))
-    love.graphics.polygon(mode, objects.left_wall.body:getWorldPoints(
-                              objects.left_wall.shape:getPoints()))
-    love.graphics.polygon(mode, objects.right_wall.body:getWorldPoints(
-                              objects.right_wall.shape:getPoints()))
-    love.graphics.polygon(mode, objects.ceiling.body:getWorldPoints(
-                              objects.ceiling.shape:getPoints()))
-
-    -- Switch back to normal ink
-    love.graphics.setColor(255, 255, 255, 255)
 end
 
 ------ None of these are used yet -------
